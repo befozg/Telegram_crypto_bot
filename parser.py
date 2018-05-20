eb0a6dd274d4239397692b810e57c2ba8fbcb742
Karen-1999 <32958661+Karen-1999@users.noreply.github.com>
2018-05-20 19:08:53 +0300

    format datetime

--- parser.py
+++ parser.py
@@ -45,8 +45,8 @@ def crate(crypt_codes='USD', crypt_to='RUB'):
 
 
 @ConvertArgumentTypes(str, int, int, str, str)
-def history(crypt_codes="USD", begin_time=datetime.strptime('2018.04.01', '%Y.%m.%d'), 
-            end_time=datetime.strptime('2018.04.11', '%Y.%m.%d'), resolution='day', crypt_to='RUB'):
+def history(crypt_codes="USD", begin_time=datetime(2018,4,1), 
+            end_time=datetime(2018, 4, 11), resolution='day', crypt_to='RUB'):
     """
     С помощью специальных библиотек парсит информацию с указанной ссылки, и 
     по следующим аргументам выводит историю изменения курса:
