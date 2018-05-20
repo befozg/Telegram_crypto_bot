86a5bdf883ab2b11ffdc44f48bdf460589f9d094
Karen-1999 <32958661+Karen-1999@users.noreply.github.com>
2018-05-20 19:50:09 +0300

    defargs

--- parser.py
+++ parser.py
@@ -55,18 +55,21 @@ def history(crypt_codes: str, begin_time: int, end_time: int, resolution: str, c
     :param crypt_to=USD: Крипт, в который надо перевести 
     :return: текущее значение crypt в эквивалентах crypt_to
     """
-    api_res = {'minute': 1, 'hour': 60, 'day': 1440}
-    limit = (end_time - begin_time) // (api_res[resolution] * 60)
-    request = 'https://min-api.cryptocompare.com/data/histo' + resolution + '?fsym=' + crypt_codes + '&tsym=' + crypt_to + '&limit=' + str(
-        limit) + '&toTs=' + str(end_time)
-    response = json.loads(urllib.request.urlopen(request).read().decode('utf-8'))
-    dates = matplotlib.dates.date2num(list(map(lambda x: datetime.datetime.fromtimestamp(x['time']), response['Data'])))
-    values = list(map(lambda x: x['close'], response['Data']))
-    mat_pyplot.scatter(dates, values)
-    mat_pyplot.plot_date(dates, values, '-o')
-    mat_pyplot.gcf().autofmt_xdate()
-    mat_pyplot.title(crypt_codes + ' currency')
-    mat_pyplot.xlabel('Date')
-    mat_pyplot.ylabel(crypt_codes + ' to ' + crypt_to)
-    mat_pyplot.savefig('tmp_fig.png')
-    mat_pyplot.close()
+     try:
+        api_res = {'minute': 1, 'hour': 60, 'day': 1440}
+        limit = (end_time - begin_time) // (api_res[resolution] * 60)
+        request = 'https://min-api.cryptocompare.com/data/histo' + resolution + '?fsym=' + crypt_codes + '&tsym=' + crypt_to + '&limit=' + str(
+            limit) + '&toTs=' + str(end_time)
+        response = json.loads(urllib.request.urlopen(request).read().decode('utf-8'))
+        dates = matplotlib.dates.date2num(list(map(lambda x: datetime.datetime.fromtimestamp(x['time']), response['Data'])))
+        values = list(map(lambda x: x['close'], response['Data']))
+        mat_pyplot.scatter(dates, values)
+        mat_pyplot.plot_date(dates, values, '-o')
+        mat_pyplot.gcf().autofmt_xdate()
+        mat_pyplot.title(crypt_codes + ' currency')
+        mat_pyplot.xlabel('Date')
+        mat_pyplot.ylabel(crypt_codes + ' to ' + crypt_to)
+        mat_pyplot.savefig('tmp_fig.png')
+        mat_pyplot.close()
+    except Exception:
+        print('Вводить надо в формате /history и аргументы :)')
